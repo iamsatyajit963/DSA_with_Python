318eb108ab98e523df6ee9f86605d74727aadb36
Satyajit <satyajitsenapati1996@gmail.com>
2024-03-21 13:11:51 +0530

    Update department_in_us.sql

--- sql/department_in_us.sql
+++ sql/department_in_us.sql
@@ -6,6 +6,15 @@ Note:
 
 Return the columns employee_id, first_name, and last_name.
 Return the result ordered by employee_id in ascending order.
+Table Name: employees
+Schema:
+Departmant [departmane_id, department_name, manager_id, location_id]
+Employees [employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, commission_pct, manager_id, department_id]
+Locations [location_id, street_address, postal_code, city, state_province, country_id]
+Countries [country_id, country_name, region_id]
+Regions [region_id, region_name]
+Job_history [employee_id, start_date, end_date, job_id, department_id]
+Jobs [job_id, job_title, min_salary, max_salary]
 **/
 select 
 e.employee_id, e.first_name, e.last_name 
