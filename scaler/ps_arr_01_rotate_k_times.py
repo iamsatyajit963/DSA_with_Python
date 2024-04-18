32af4ec907417e4b4e8aaa2e75a0b52a307d7046
Satyajit <satyajitsenapati1996@gmail.com>
2024-04-18 13:02:13 +0530

    Update ps_arr_01_rotate_k_times.py

--- scaler/ps_arr_01_rotate_k_times.py
+++ scaler/ps_arr_01_rotate_k_times.py
@@ -1,16 +1,16 @@
 '''
 Problem Description
-Given an integer array A of size N and an integer B, you have to print the same array after rotating it B times towards the right.
+Given an integer array arr of size N and an integer K, you have to print the same array after rotating it K times towards the right.
 Problem Constraints
 1 <= N <= 106
 1 <= A[i] <=108
 1 <= B <= 109
 Input Format
 There are 2 lines in the input
-Line 1: The first number is the size N of the array A. Then N numbers follow which indicate the elements in the array A.
-Line 2: A single integer B.
+Line 1: The first number is the size N of the array arr. Then N numbers follow which indicate the elements in the array arr.
+Line 2: A single integer K.
 Output Format
-Print array A after rotating it B times towards the right.
+Print array arr after rotating it K times towards the right.
 Example Input
 Input 1 :
 4 1 2 3 4
@@ -20,7 +20,7 @@ Output 1 :
 3 4 1 2
 Example Explanation
 Example 1 :
-N = 4, A = [1, 2, 3, 4] and B = 2.
+N = 4, arr = [1, 2, 3, 4] and K = 2.
 Rotate towards the right 2 times - [1, 2, 3, 4] => [4, 1, 2, 3] => [3, 4, 1, 2]
 Final array = [3, 4, 1, 2]
 '''
