b04b7bfcab5329428e11294e822ed10485682539
Satyajit <satyajitsenapati1996@gmail.com>
2024-07-09 15:55:41 +0530

    Update ps_03_closest_minmax.py

--- scaler/carryforward/ps_03_closest_minmax.py
+++ scaler/carryforward/ps_03_closest_minmax.py
@@ -19,10 +19,18 @@ Example Explanation
 Explanation 1: Take the 1st and 2nd elements as they are the minimum and maximum elements respectievly.
 Explanation 2: Take the last 3 elements of the array.
 '''
+import sys
 A = [1,3,2]
 min_val = min(A)
 max_val = max(A)
-min_index = A.index(min_val)
-max_index = A.index(max_val)
-subarray_length = abs(max_index - min_index) + 1
-print(subarray_length)
+
+min_index, max_index, ans = -1, -1, sys.maxsize
+for i in range(0, len(A)):
+    if A[i] == min_val:
+        min_index = i
+    if A[i] == max_val:
+        max_index = i
+    if min_index != -1 and max_index != -1:
+        ans = min(ans, abs(max_index - min_index) + 1)
+
+print(ans)
